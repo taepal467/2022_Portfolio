0af1f1ffea19fd3424a35d91f8664871155d1909
Chantae P <60330899+taepal467@users.noreply.github.com>
2022-05-24 20:45:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 const menuBtn = document.querySelector(".menu-btn");
 const menuBurger = document.querySelector(".menu-burger");
 const nav = document.querySelector(".nav");
-const navMemu = document.querySelector(".nav-menu");
+const navMenu = document.querySelector(".nav-menu");
 const navItems = document.querySelectorAll(".nav-item");
 
-let showMemu = false;
+let showMenu = false;
 
 const toggleMenu = () => {
     if (!showMemu) {
         menuBurger.classList.add("open");
         nav.classList.add("open");
-        navMemu.classList.add("open");
+        navMenu.classList.add("open");
         navItems.forEach(item => item.classList.add("open"));
 
-        showMemu = true;
+        showMenu = true;
     } else {
         menuBurger.classList.remove("open");
         nav.classList.remove("open");
-        navMemu.classList.remove("open");
+        navMenu.classList.remove("open");
         navItems.forEach(item => item.classList.remove("open"));
 
-        showMemu = false;
+        showMenu = false;
     }
 }
 
